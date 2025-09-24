189b4f3d079178adb9730f78e23f9334e1a144dc
alex brosovich <alexbrosovich@gmail.com>
2025-09-24 15:49:16 -0400

    mongoose

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const { PORT = 3001 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connect to DB");
+  })
+  .catch(console.error);
