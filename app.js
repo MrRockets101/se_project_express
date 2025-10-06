216a35d15aed92050908248fd73dc7876f122a41
alex brosovich <alexbrosovich@gmail.com>
2025-10-06 09:50:23 -0400

    comment notes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-// Add logging middleware here
+//  debug
 app.use((req, res, next) => {
   console.log(`Incoming request: ${req.method} ${req.url}`);
   next();
