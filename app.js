6257050f0222f13d860ee78e2f7c4f0b118a3510
alex brosovich <alexbrosovich@gmail.com>
2025-10-10 16:21:51 -0400

    crash-test

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ app.get("/api/weather", async (req, res) => {
 });
 
 app.use(requestLogger);
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 app.use("/", mainRouter);
 app.use(errorLogger);
 app.use(errors()); // Handles Celebrate/Joi errors
